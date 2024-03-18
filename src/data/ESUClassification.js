6de7d0b6fff7d6192988b33ca242ec40a80f50c9
Sean-Idox-ADS <sean.flook@idoxgroup.com>
2024-03-18 10:47:33 +0000

    ESU2_OS Set avatar colour

--- src/data/ESUClassification.js
+++ src/data/ESUClassification.js
@@ -1,27 +1,35 @@
+import { adsPurple } from "../utils/ADSColours";
+
 const ESUClassification = [
   {
     id: 4,
     osText: "Pedestrian way or footpath",
+    colour: adsPurple,
   },
   {
     id: 6,
     osText: "Cycle track",
+    colour: adsPurple,
   },
   {
     id: 8,
     osText: "Open to vehicles",
+    colour: adsPurple,
   },
   {
     id: 11,
     osText: "Motorway",
+    colour: adsPurple,
   },
   {
     id: 12,
     osText: "Special road",
+    colour: adsPurple,
   },
   {
     id: 13,
     osText: "Car park",
+    colour: adsPurple,
   },
 ];
 
--- src/data/ESUState.js
+++ src/data/ESUState.js
@@ -1,19 +1,25 @@
+import { adsPurple } from "../utils/ADSColours";
+
 const ESUState = [
   {
     id: 1,
     osText: "Under construction",
+    colour: adsPurple,
   },
   {
     id: 2,
     osText: "Open",
+    colour: adsPurple,
   },
   {
     id: 3,
     osText: "Proposed",
+    colour: adsPurple,
   },
   {
     id: 4,
     osText: "Permanently closed",
+    colour: adsPurple,
   },
 ];
 
