7014c78fd16436055ee2182d27bf5e2aedac521d
Sean-Idox-ADS <sean.flook@idoxgroup.com>
2024-11-27 07:28:43 +0000

    IMANN-1076 Modified to handle different types of date.

--- src/utils/Type10ValidationChecks.jsx
+++ src/utils/Type10ValidationChecks.jsx
@@ -12,13 +12,13 @@
 //  Version Date     Modifier            Issue# Description
 //#region Version 1.0.2.0 changes
 //    001   25.11.24 Sean Flook      IMANN-1076 Initial Revision.
+//    002   27.11.24 Sean Flook      IMANN-1076 Modified to handle different types of date.
 //#endregion Version 1.0.2.0 changes
 //
 //--------------------------------------------------------------------------------------------------
 /* #endregion header */
 
-import { isValidDate } from "./HelperUtils";
-import { parseISO } from "date-fns";
+import { isValid, parseISO } from "date-fns";
 
 /**
  * Check 1100004 - End date cannot be before the street start date.
@@ -27,5 +27,5 @@ import { parseISO } from "date-fns";
  * @returns {Boolean} True if the check fails; otherwise false.
  */
 export const failsCheck1000020 = (date) => {
-  return date && !isValidDate(parseISO(date));
+  return date && !isValid(date) && !isValid(parseISO(date));
 };
